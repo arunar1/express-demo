b331c14a454785178080c74cf326115614f183cb
Arun <arunarofficial00@gmail.com>
2024-02-05 01:28:34 +0530

    starting with middleware

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,13 @@ const express =require('express')
 
 const router=express.Router();
 
+
+
+router.use('/',(req,res,next)=>{
+    console.log("API call received");
+    next()
+})
+
 router.post('/',(req,res)=>{
     res.send("user created ")
 })
@@ -10,7 +17,16 @@ router.get('/user-details/:id',(req,res)=>{
     res.send("user details " + req.params.id);
 })
 
-router.get('/userspecific:key([a-zA-Z]{5})',(req,res)=>{
-    res.send("The specific result"+req.params.key)
+router.get('/userspecific/:key([a-zA-Z]{5})',(req,res)=>{
+    res.send("The specific result "+req.params.key)
+})
+
+
+router.get('*',(req,res)=>{
+    const errorobj={
+        errorcode:'404',
+        response:"invalid  url"
+    }
+    res.send(JSON.stringify(errorobj))
 })
 module.exports =router; 
