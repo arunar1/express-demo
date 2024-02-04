5cf71365989649957a5584236989c76ab1f252aa
Arun <arunarofficial00@gmail.com>
2024-02-04 20:40:37 +0530

    added dynamic routing

--- app.js
+++ app.js
@@ -1,65 +1,13 @@
 const express=require('express')
 const product=require('./routes/product')
 const user=require('./routes/user')
-
 const app=express();
 
  
 
 app.use('/product',product)
-
 app.use('/user',user)
 
-
-app.post('/create-user',(req,res)=>{
-    res.send("Create user")
-})
-
-
-
-
-
-
-const movies=[
-    {
-        id:1,
-        name:"Avengers",
-        year:2001,
-        rating:9.0
-    },
-    {
-        id:2,
-        name:"thor",
-        year:2004,
-        rating:9.1
-    }
-]
-
-app.get('/',(request,response)=>{
-    response.send("Get method in movies");
-});
-
-app.get('/movies',(req,res)=>{
-    res.send({
-        message1:'here are your movies my friend',
-        data1:movies
-    });
-});
-
-
-app.post("/movies",(req,res)=>{
-    movies.push(req.body);
-    res.send('we have a new movie '+ req.body.name);
-});
-
-
-app.delete('/movies',(req,res)=>{
-    const id=req.query.id;
-    console.log(id);
-    const index=movies.findIndex((movie)=>movie.id===parseInt(id));
-    movies.splice(index,1);
-    res.send('movie deleted');
-});
 
 
 
--- routes/user.js
+++ routes/user.js
@@ -6,8 +6,8 @@ router.post('/',(req,res)=>{
     res.send("user created ")
 })
 
-router.get('/user-details',(req,res)=>{
-    res.send("user details");
+router.get('/user-details/:id',(req,res)=>{
+    res.send("user details " + req.params.id);
 })
 
 module.exports =router;
