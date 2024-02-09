d7b1f4ee3bc329a7228b3feb87f16678b7531ce1
Arun <arunarofficial00@gmail.com>
2024-02-10 00:23:49 +0530

    concept of dynamic

--- Express-Generator/routes/index.js
+++ Express-Generator/routes/index.js
@@ -7,7 +7,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/express',(req,res,next)=>{
-  res.render('index',{title:'Express',subtitle:'This is developed for study purpose'})
+  console.log(req.query.course)
+  res.render('index',{title:'Express',subtitle:`This is developed for study purpose and ${req.query.course.toUpperCase()} is the fundamental`})
 });
 
 module.exports = router;
