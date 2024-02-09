ac9f9e3ba0fc1432cade1e2c0198da83cf4af00b
Arun <arunarofficial00@gmail.com>
2024-02-09 23:16:49 +0530

    added the nodemon package and routing

--- Express-Generator/routes/index.js
+++ Express-Generator/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello' });
+  res.render('index', { title: 'Backend' });
+});
+
+router.get('/express',(req,res,next)=>{
+  res.render('index',{title:'Express',subtitle:'This is developed for study purpose'})
 });
 
 module.exports = router;
